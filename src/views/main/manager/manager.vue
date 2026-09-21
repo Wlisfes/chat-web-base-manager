11ea85e99b9d06c3681af387b9559791c98e2e16
妖雨纯 <876451336@qq.com>
2026-09-21 14:56:43 +0800

    feat: 添加 common-base-chunk 组件到管理视图，优化颜色类型展示

--- src/views/main/manager/manager.vue
+++ src/views/main/manager/manager.vue
@@ -5,8 +5,31 @@ export default defineComponent({
     name: 'BaseManager',
     setup() {
         return () => (
-            <layout-common-container class="absolute inset-0" class-name="flex-col overflow-hidden">
-                <deploy-system-dept-orgchart></deploy-system-dept-orgchart>
+            <layout-common-container class="absolute inset-0 flex-wrap">
+                <div class="flex flex-wrap gap-10 p-20">
+                    {[
+                        'default',
+                        'primary',
+                        'info',
+                        'success',
+                        'warning',
+                        'error',
+                        'red',
+                        'orange',
+                        'green',
+                        'cyan',
+                        'blue',
+                        'geekblue',
+                        'purple',
+                        'pink',
+                        'volcano',
+                        'gray'
+                    ].map(type => (
+                        <common-base-chunk bordered value type={type}>
+                            {type}
+                        </common-base-chunk>
+                    ))}
+                </div>
             </layout-common-container>
         )
     }
